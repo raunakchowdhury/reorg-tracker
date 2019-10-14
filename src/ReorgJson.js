5716368f7d4901ce1f1dca87e1cd5ba8ee243cd0
Nicholas Yang <ny585@nyu.edu>
2019-10-14 17:49:18 -0400

    Update ReorgJson.js

--- src/ReorgJson.js
+++ src/ReorgJson.js
@@ -8,18 +8,11 @@ const ReorgJson = (props) => {
      * Functional component.
      */
 
-    const [data, setData] = useState({});
     const [reorg_data, setReorgData] = useState({});
 
     async function getData(){
         const res = await fetch(props.request);
         let resJson = await res.json();
-        // setData(resJson);
-        // console.log(resJson, props.request);
-        return resJson;
-    };
-
-    function getReorgs(){
         /*
          * Returns the reorgs of the data in the form of a dictionary.
          * Returned dict mapping:
@@ -41,37 +34,20 @@ const ReorgJson = (props) => {
             reorgs.reorg_length.push(data[i].event.removed_blocks.length);
             reorgs.timestamps.push(data[i].event.added_blocks.slice(-1)[0].receipt_time);
         };
-
-        console.log(reorgs);
         setReorgData(reorgs);
     };
 
     useEffect(() => {
-        let getData = async () => {
-            const res = await fetch(props.request);
-            let resJson = await res.json();
-            setData(resJson);
-            console.log(resJson); // , props.request);
-            // return resJson;
-        };
-
         getData();
-        getReorgs();
-    }, []);
-
-    useEffect(() => {
-
-        // console.log(data, reorg_data);
-
         // lifecycle method
         const interval = setInterval( () => {
             getData();
-            getReorgs();
+
         }, 5000);
 
         // when unmounted, clear the interval
         return () => clearInterval(interval);
-    });
+    }, []);
 
     return <ReorgTable data = {reorg_data} />;
     // return <div>{JSON.stringify(reorg_data)}</div>;
