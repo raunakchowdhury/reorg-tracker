b8faab8ff7eb16de9c9499abd8d71a97b01268c0
Raunak Chowdhury <raunakchowdhury@Raunaks-MacBook-Pro.local>
2019-10-14 21:44:45 -0400

    added table functionality

--- src/ReorgJson.js
+++ src/ReorgJson.js
@@ -4,77 +4,64 @@ import ReorgTable from "./ReorgTable";
 
 const ReorgJson = (props) => {
     /*
-     * Handles all the API requests and returns the raw JSON.
+     * Handles the reorg API request and generates the data for the reorgs.
      * Functional component.
      */
 
-    const [data, setData] = useState({});
-    const [reorg_data, setReorgData] = useState({});
+    const [reorg_data, setReorgData] = useState([]);
 
-    async function getData(){
-        const res = await fetch(props.request);
-        let resJson = await res.json();
-        // setData(resJson);
-        // console.log(resJson, props.request);
-        return resJson;
-    };
-
-    function getReorgs(){
+    async function getData() {
         /*
-         * Returns the reorgs of the data in the form of a dictionary.
-         * Returned dict mapping:
-         * {
-         *  timestamps : [ list of timestamps ]
-         *  reorg_lengths: [ list of reorg lengths ]
-         *  names: [ list of networks affected by the reorg ]
-         * }
-         * Index mapping is used to keep all the reorgs in order (so timestamps[i], reorg_lengths[i], names[i] all refer to the same reorg).
+         * Returns the reorgs of the data in the form of a list.
+         * Returned list mapping:
+         * [
+         *  timestamp : [ list of timestamps ]
+         *  reorg_length: [ list of reorg lengths ]
+         *  name: [ list of networks affected by the reorg ]
+         * ]
          */
-        let reorgs = {
-            timestamps: [],
-            reorg_length: [],
-            names: []
-        };
+
+        const res = await fetch(props.request);
+        let data = await res.json();
+
+
+        let reorgs = [];
 
         for (let i = 0; i < data.length; i++){
-            reorgs.names.push(data[i].currency_name);
-            reorgs.reorg_length.push(data[i].event.removed_blocks.length);
-            reorgs.timestamps.push(data[i].event.added_blocks.slice(-1)[0].receipt_time);
+            reorgs.push({
+                id: i,
+                name: data[i].currency_name,
+                reorg_length: data[i].event.removed_blocks.length,
+                timestamp: data[i].event.added_blocks.slice(-1)[0].receipt_time * 1000
+            });
         };
 
-        console.log(reorgs);
         setReorgData(reorgs);
     };
 
     useEffect(() => {
-        let getData = async () => {
-            const res = await fetch(props.request);
-            let resJson = await res.json();
-            setData(resJson);
-            console.log(resJson); // , props.request);
-            // return resJson;
-        };
 
+        // effect triggers once, on mount
         getData();
-        getReorgs();
-    }, []);
-
-    useEffect(() => {
-
-        // console.log(data, reorg_data);
 
         // lifecycle method
         const interval = setInterval( () => {
             getData();
-            getReorgs();
         }, 5000);
 
         // when unmounted, clear the interval
         return () => clearInterval(interval);
-    });
+    }, []);
 
-    return <ReorgTable data = {reorg_data} />;
-    // return <div>{JSON.stringify(reorg_data)}</div>;
+    return (
+        // add multiple elements to DOM
+        <>
+            <div key={reorg_data}>
+                <ReorgTable data = {reorg_data} />
+            </div>
+        </>
+
+    );
 };
 
 export default ReorgJson;
--- src/ReorgTable.js
+++ src/ReorgTable.js
@@ -1,49 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const ReorgTable = (props) => {
     /*
      * Generates the table based on the data passed into it.
      */
 
-    const [jsx_data, changeData] = useState([]);
+    let jsx_data = [
+        <tr key="header-row">
+            <th key="time">Time</th>
+            <th key="name">Name</th>
+            <th key="length">Length of Reorg</th>
+        </tr>
+    ];
 
-    useEffect(() => {
-        generate();
-    }, []);
-
-
-    // useEffect( () => {
-    //     generate();
-    // }, ]);
-
-    function generate(){
-        /*
-         * Generates the JSX array.
-         */
-
-        console.log(props.data);
-        let table_array = [
-            <tr key="header-row">
-                <th key="time">Time</th>
-                <th key="name">Name</th>
-                <th key="length">Length of Reorg</th>
+    for (let row = 0; row < props.data.length; row++){
+        // console.log(props.data[row].timestamp, props.data[row].name, props.data[row].reorg_length);
+        jsx_data.push(
+            <tr key={`header_${row}`}>
+                <td key={`timestamp_${row}`}>{`${new Date(props.data[row].timestamp).toLocaleDateString("en-US")} ${new Date(props.data[row].timestamp).toLocaleTimeString("en-US")}`}</td>
+                <td key={`name_${row}`}>{props.data[row].name}</td>
+                <td key={`reorg_${row}`}>{props.data[row].reorg_length}</td>
             </tr>
-        ];
-
-        for (let row = 0; row < props.data.length; row++){
-            table_array.push(
-                <tr key="header_{row}">
-                    <td key="timestamp_{row}">{props.data.timestamps[row]}</td>
-                    <td key="name_{row}">{props.data.names[row]}</td>
-                    <td key="reorg_{row}">{props.data.reorg_length[row]}</td>
-                </tr>
-            );
-        };
-
-        changeData(table_array);
+        );
     };
 
-    // generate();
+
     return (
         <table>
             <tbody>{jsx_data}</tbody>
